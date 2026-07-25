800f86409296105af2dc6bab4fd946b84d11bb36
Fazubre <fazubre@gmail.com>
2026-07-24 18:08:23 -0600

    Ahora si, ruta frfr

--- Frontend/js/tickets/tickets.modal.js
+++ Frontend/js/tickets/tickets.modal.js
@@ -47,7 +47,7 @@ const MODAL_CONFIG = [
             "modalAgregarTicket",
 
         ruta:
-            `${COMPONENTS_BASE_URL}/modal-add-ticket.html`
+            `${COMPONENTS_BASE_URL}/Tickets/modal-add-ticket.html`
     },
     {
         contenedorId:
@@ -57,7 +57,7 @@ const MODAL_CONFIG = [
             "modalVerTicket",
 
         ruta:
-            `${COMPONENTS_BASE_URL}/modal-view-ticket.html`
+            `${COMPONENTS_BASE_URL}/Tickets/modal-view-ticket.html`
     },
     {
         contenedorId:
@@ -67,7 +67,7 @@ const MODAL_CONFIG = [
             "modalEditarTicket",
 
         ruta:
-            `${COMPONENTS_BASE_URL}/modal-edit-ticket.html`
+            `${COMPONENTS_BASE_URL}/Tickets/modal-edit-ticket.html`
     },
     {
         contenedorId:
@@ -77,7 +77,7 @@ const MODAL_CONFIG = [
             "modalComentariosTicket",
 
         ruta:
-            `${COMPONENTS_BASE_URL}/modal-comments-ticket.html`
+            `${COMPONENTS_BASE_URL}/Tickets/modal-comments-ticket.html`
     }
 ];
 
